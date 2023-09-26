cfcc88885635b011f2dfcc9c949422527e1a9670
Arsalan <arsalanmohd020@gmail.com>
2023-09-26 15:48:41 +0530

    hi navbar change

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -12,9 +12,9 @@ return (
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+      <ul className="navbar-nav me-auto mb-3 mb-lg-0">
         <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="#">Home</a>
+          <a className="nav-link active" aria-current="page" href="#">House</a>
         </li>
         <li className="nav-item">
           <a className="nav-link" href="/">{props.AboutText}</a>
